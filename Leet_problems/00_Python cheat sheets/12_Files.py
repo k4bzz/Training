03f3867c7f6b2c98c561c094f35a2347ba38b9f0
Yojee <N/A>
2022-12-11 17:43:15 +0000

    Started files

---
+++ Leet_problems/00_Python cheat sheets/12_Files.py
@@ -0,0 +1,11 @@
+# FILES
+
+"""
+https://www.codecademy.com/learn/learn-python-3/modules/learn-python3-files/cheatsheet
+"""
+
+# TODO learn context managers
+# OPEN FILE WITH CONTEXT MANAGER
+with open('test_file.txt') as cool_doc:
+    cool_contents = cool_doc.read()
+print(cool_contents)
