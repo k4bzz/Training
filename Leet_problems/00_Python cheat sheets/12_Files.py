b5cad03c1dcaabaea235e7c95523baab084ad009
Yojee <N/A>
2022-12-30 20:39:12 +0000

    Continue with files CSV

--- Leet_problems/00_Python cheat sheets/12_Files.py
+++ Leet_problems/00_Python cheat sheets/12_Files.py
@@ -65,3 +65,24 @@ with open('test2.csv', newline='') as addresses_csv:
     address_reader = csv.DictReader(addresses_csv, delimiter=';')
     for row in address_reader:
         print(row['Address'])
+
+# Write list into a file
+big_list = [{'name': 'Fredrick Stein', 'userid': 6712359021, 'is_admin': False},
+            {'name': 'Wiltmore Denis', 'userid': 2525942, 'is_admin': False},
+            {'name': 'Greely Plonk', 'userid': 15890235, 'is_admin': False},
+            {'name': 'Dendris Stulo', 'userid': 572189563, 'is_admin': True}]
+
+import csv
+
+with open('output.csv', 'w') as output_csv:
+    fields = ['name', 'userid', 'is_admin']
+    output_writer = csv.DictWriter(output_csv, fieldnames=fields)
+
+    output_writer.writeheader()
+    for item in big_list:
+        output_writer.writerow(item)
+
+with open("output.csv", newline='') as fileX:
+    output_writer2 = csv.DictReader(fileX)
+    for row in output_writer2:
+        print(row)
