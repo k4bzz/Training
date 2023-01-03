512d04d89fac63cd9d113f4743dd77b80a0fdb35
Yojee <basubsbox@gmail.com>
2023-01-03 20:30:28 +0000

    added venv and .idea in the scope

--- Leet_problems/98_CA_Challenges/5_Dictionaries_Adv.py
+++ Leet_problems/98_CA_Challenges/5_Dictionaries_Adv.py
@@ -6,6 +6,7 @@ def word_length_dictionary(words: list) -> dict:
     new_dict = dict(zip(words, lengths))
     return new_dict
 
+
 print(word_length_dictionary(["apple", "dog", "cat"]))
 print(word_length_dictionary(["a", ""]))
 
@@ -19,3 +20,31 @@ def word_length_dictionary(words):
     return word_lengths
 """
 
+
+# Creates a dictionary with frequency of words in the list
+def frequency_dictionary(words: list) -> dict:
+    freq_dict = {}
+    for word in words:
+        freq_dict[word] = words.count(word)
+    return freq_dict
+
+
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+print(frequency_dictionary([0, 0, 0, 0, 0]))
+
+# Return number of unique values from dictionary
+def unique_values(my_dictionary: dict) -> int:
+    counter = 0
+    # print(list(my_dictionary.values()))
+
+    for value in my_dictionary.values():
+        print(f" value {value} count {list(my_dictionary.values()).count(value)}")
+        if list(my_dictionary.values()).count(value) == 1:
+            counter += 1
+    return counter
+
+
+print(unique_values({0: 3, 1:1, 4:1, 5:3}))
+print(unique_values({0:3, 1:3, 4:3, 5:3}))
+
+# print(dir(list))
