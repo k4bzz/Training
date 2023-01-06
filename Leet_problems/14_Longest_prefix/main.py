53e7ee851ef9aea38f8204173f5d273e03a5e3fe
Yojee <basubsbox@gmail.com>
2023-01-06 20:39:17 +0000

    gitignore added to .idea ~/leet/folder

--- Leet_problems/14_Longest_prefix/main.py
+++ Leet_problems/14_Longest_prefix/main.py
@@ -16,3 +16,4 @@ Explanation: There is no common prefix among the input strings.
 # excluded from sources - from banner - didnt work
 # added to exclusion in structure - nope
 # adding /path to gitignore - nope
+# ???
